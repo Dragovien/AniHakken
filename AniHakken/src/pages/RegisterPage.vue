a372d4e001c2ed59ea6be952f52a76deff2f3c69
axel hegron <axel@miloctav.fr>
2024-03-22 14:06:11 +0100

    added login, register, forgotten password page

---
+++ AniHakken/src/pages/RegisterPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="page">
+  
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'RegisterPage',
+  data () {
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- AniHakken/src/router/routes.js
+++ AniHakken/src/router/routes.js
@@ -1,26 +1,49 @@
 
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', name: 'IndexPage', component: () => import('pages/IndexPage.vue') },
-    ]
+      {
+        path: "",
+        name: "IndexPage",
+        component: () => import("pages/IndexPage.vue"),
+      },
+      {
+        path: "login",
+        name: "LoginPage",
+        component: () => import("pages/LoginPage.vue"),
+      },
+      {
+        path: "register",
+        name: "RegisterPage",
+        component: () => import("pages/RegisterPage.vue"),
+      },
+      {
+        path: "forgot-password",
+        name: "ForgottenPasswordPage",
+        component: () => import("pages/ForgottenPasswordPage.vue"),
+      },
+    ],
   },
   {
-    path: '/anime',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/anime",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: ':id', name: 'AnimeShow', component: () => import('pages/AnimeDetailsPage.vue') },
-    ]
+      {
+        path: ":id",
+        name: "AnimeShow",
+        component: () => import("pages/AnimeDetailsPage.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
 export default routes
